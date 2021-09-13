5cd9db664256e79a14299b6d91e1d934c07053c1
dngruman <90514292+dngruman@users.noreply.github.com>
2021-09-12 20:57:53 -0400

    Initial commit

---
+++ src/main/java/SelfCheckout.java
@@ -0,0 +1,28 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Deirdre Gruman-Walsh
+ */
+import java.util.Scanner;
+public class SelfCheckout {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        float price[] = new float[3];
+        float quantity[] = new float[3];
+        float subtotal = 0;
+
+        for(int i = 0; i < 3; i++){
+            System.out.printf("Enter the price of item %d: ", (i+1));
+            price[i] = user_input.nextInt();
+            System.out.printf("Enter the quantity of items %d: ", (i+1));
+            quantity[i] = user_input.nextInt();
+        }
+
+        for(int i = 0; i < 3; i++){
+            subtotal += price[i] * quantity[i];
+        }
+        System.out.println("Subtotal: $" + String.format("%.2f",subtotal));
+        System.out.println("Tax: $" + String.format("%.2f",subtotal * 0.055));
+        System.out.println("Total: $" + String.format("%.2f",subtotal + (subtotal * 0.055)));
+    }
+    }
+
